87973deaf1a342713a1c2a9329f46a995831383d
Федорченко Олег Васильевич <OVFedorchenko@sberbank.ru>
2024-08-23 16:06:21 +0300

    update oak sh

--- oak/oak.sh
+++ oak/oak.sh
@@ -7,7 +7,7 @@ service="jazz-$4"
 
 if [[ $1 == "-h" || $1 == "-help" ]]; then
     echo "usage ./oak.sh cfg_name[dev05|cce] name_space[oak|dev06] type[pods|log|sh|del] service_name"
-    echo -e "example for sip-manager log on dev05:\n./oak.sh dev05 oak log sip-manager"
+    echo -e "example for sip-manager log on dev05:\n./oak.sh dev05 dev05 log sip-manager"
     exit 0
 fi
 
@@ -29,14 +29,14 @@ container_name=$(kubectl --kubeconfig $cfg -n $ns get pods | rg $service | awk '
 
 echo "Try connecting using container name: $container_name"
 
+
 if [[ $type == "log" ]]; then
 
-    cmd="kubectl --kubeconfig $cfg -n $ns logs -f --tail=7000 $container_name \
-    | zap-pretty | sed -u 's/\\n/\n/g' | sed -u 's/\\"/"/g'  | sed -u 's/\\r//g'"
+    cmd="kubectl --kubeconfig $cfg -n $ns logs -f --tail=7000 $container_name "
 
     echo "cmd: $cmd"
 
-    eval $cmd
+    eval $cmd | zap-pretty | sed -u 's/\\n/\n/g' | sed -u 's/\\"/"/g' | sed -u 's/\\r//g' | sed -u 's/\\//g'
 
 elif [[ $type == "sh" ]]; then
 
